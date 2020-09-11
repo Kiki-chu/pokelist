a6b55255a05c3e0d6aebd9f4242051618775e681
Kiki <kiki_de_bruijn@hotmail.com>
2020-09-11 17:50:37 +0100

    now with loading screen and working buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
+import Pages from "./Pages";
+// Axios is a library that allows for easier use fetch requests
+import axios from "axios";
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
-  return <PokemonList pokemon={pokemon} />;
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    // cancel token in case useEffect gets called multiple times before the request is finished. A clean slate
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((pokemon) => pokemon.name));
+      });
+    // clean slate
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  function nextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function prevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  if (loading) return "Loading...!";
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pages
+        gotoNextPage={nextPageUrl ? nextPage : null}
+        // only if there is a previous page
+        gotoPrevPage={prevPageUrl ? prevPage : null}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/Pages.js
+++ src/Pages.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Pages() {
-  return <div></div>;
+export default function Pages({ gotoNextPage, gotoPrevPage }) {
+  return (
+    <div>
+      {/* cheeky way to do an if statement in React */}
+      {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
+      {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+    </div>
+  );
 }
